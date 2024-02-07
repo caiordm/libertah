e692f94d9f154b9d1902c99a855d90491da84c23
caiordm <caiordm08@gmail.com>
2024-02-07 20:20:27 -0300

    test analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
     import logo from "../images/logoTransparentWhite.png";
 
-    // Import the Analytics package, and the SvelteKit dev variable.
     import { dev } from "$app/environment";
     import { inject } from "@vercel/analytics";
 
-    // Inject the Analytics functionality
     inject({ mode: dev ? "development" : "production" });
 </script>
 
